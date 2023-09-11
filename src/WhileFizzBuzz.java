2429f0ca6a2a0029e05e36ba2be2c7e81f2ad704
youyang <youyang.guo@mail.utoronto.ca>
2023-09-11 18:51:50 -0400

    lab1

--- src/WhileFizzBuzz.java
+++ src/WhileFizzBuzz.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class whilefizzbuzz {
+public class WhileFizzBuzz {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 100) {
+            i = doFizzBuzz(i);
+        }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+        }
+        i++;
+        return i;
+    }
 }
+

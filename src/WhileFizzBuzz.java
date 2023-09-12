1c6706ffa5c3bc041708a3b427a0dc6cd4f54197
youyang <youyang.guo@mail.utoronto.ca>
2023-09-11 22:19:13 -0400

    WFB change

--- src/WhileFizzBuzz.java
+++ src/WhileFizzBuzz.java
@@ -6,7 +6,7 @@ public class WhileFizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
         if (divisibleBy3 && divisibleBy5) {

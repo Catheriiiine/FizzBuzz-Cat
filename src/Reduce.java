8545cdafd8784d9b599cdec5b5c37848135b88c0
youyang <youyang.guo@mail.utoronto.ca>
2023-09-11 23:07:16 -0400

    task5 MarkUs

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 0;
+        while (a > 0) {
+            if (a % 2 == 0) {
+                a = a / 2;
+            } else {
+                a = a - 1;
+            }
+            b++;
+        }
+        System.out.println(b);
+    }
 }
